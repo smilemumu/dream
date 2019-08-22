6e2814f9ab931217a7529044aa35eba64103d662
shi <zhongcaishi@blcakfish.cn>
2019-08-22 15:59:50 +0800

    2019-08-22梦想开始的地方

---
+++ dream-start/src/main/java/com/shizhongcai/business/DreamStart.java
@@ -0,0 +1,13 @@
+package com.shizhongcai.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamStart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamStart.class, args);
+	}
+
+}
---
+++ dream-start/src/test/java/com/shizhongcai/business/DreamStartTests.java
@@ -0,0 +1,16 @@
+package com.shizhongcai.business;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DreamStartTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
